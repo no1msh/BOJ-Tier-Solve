38b08d5e69ae43e292ea357ae2d64433e6e667da
moon <no1msh1217@gmail.com>
2022-09-13 23:44:04 +0900

    No_9316 : Hello Judge!

---
+++ src/bronze4/No_9316.java
@@ -0,0 +1,15 @@
+package bronze4;
+
+import java.io.*;
+
+public class No_9316 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testCase = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= testCase; i++) {
+            System.out.println("Hello World, Judge " + i + "!");
+        }
+    }
+}
