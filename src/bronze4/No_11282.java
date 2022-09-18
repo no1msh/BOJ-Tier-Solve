7263a9d66e40da4950c31f2171c8072140bfe5fb
moon <no1msh1217@gmail.com>
2022-09-18 21:44:22 +0900

    No_11282 : 한글

---
+++ src/bronze4/No_11282.java
@@ -0,0 +1,14 @@
+package bronze4;
+
+import java.io.*;
+
+public class No_11282 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int inputNum = Integer.parseInt(br.readLine());
+
+        System.out.printf("%c", (inputNum + 44031));
+    }
+
+}
