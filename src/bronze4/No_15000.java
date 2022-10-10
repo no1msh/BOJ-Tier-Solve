7991735704f5274e91bdb10a97da5a4bf25f084e
moon <no1msh1217@gmail.com>
2022-10-10 09:33:47 +0900

    No_15000 : CAPS

---
+++ src/bronze4/No_15000.java
@@ -0,0 +1,12 @@
+package bronze4;
+
+import java.io.*;
+import java.util.Locale;
+
+public class No_15000 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String inputStr = br.readLine();
+        System.out.print(inputStr.toUpperCase(Locale.ROOT));
+    }
+}
