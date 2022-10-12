1b03ee557f6f1f95dcadc2a5bfc047426826dd9a
moon <no1msh1217@gmail.com>
2022-10-12 22:57:34 +0900

    No_21598 : SciComLove

---
+++ src/bronze5/No_21598.java
@@ -0,0 +1,13 @@
+package bronze5;
+
+import java.io.*;
+
+public class No_21598 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCase = Integer.parseInt(br.readLine());
+        while (testCase-- > 0) {
+            System.out.println("SciComLove");
+        }
+    }
+}
